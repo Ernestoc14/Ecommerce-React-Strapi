f3c76497eb0f0a48c3318634f338a1319456ba28
Ernestoc14 <descreed34@hotmail.com>
2023-08-01 23:56:16 -0500

    Feat Add Typography

--- client/src/theme.js
+++ client/src/theme.js
@@ -54,5 +54,21 @@ export const theme = createTheme({
     typography: {
         fontFamily: ["Fauna One" , "sans-serif"].join(","),
         fontSize: 11,
+        h1: {
+            fontFamily: ["Cinzel", "sans-serif"].join(","),
+            fontSize: 48,
+        },
+        h2: {
+            fontFamily: ["Cinzel", "sans-serif"].join(","),
+            fontSize: 36,
+        },
+        h3: {
+            fontFamily: ["Cinzel", "sans-serif"].join(","),
+            fontSize: 20,
+        },
+        h4: {
+            fontFamily: ["Cinzel", "sans-serif"].join(","),
+            fontSize: 14,
+        },
     }
 })
