3657d51c08d63fd0fcc1be03213ff3accb2e4828
Ernestoc14 <descreed34@hotmail.com>
2023-08-02 00:09:21 -0500

    Feat Setting up React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import Home from "./scenes/home/Home"
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null
+}
+
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
