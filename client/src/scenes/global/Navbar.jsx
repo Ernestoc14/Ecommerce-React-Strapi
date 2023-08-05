cf46363ea952954984a9ab2f2167e2f71e018d31
Ernestoc14 <descreed34@hotmail.com>
2023-08-05 02:42:18 -0500

    Feat Adding Badge into Navbar and Redux Provider to the Index File

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -3,10 +3,12 @@ import { Badge, Box, IconButton } from "@mui/material"
 import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined } from "@mui/icons-material"
 import { useNavigate } from "react-router-dom";
 import { shades } from "../../theme"
+import { setIsCartOpen } from "../../state";
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart)
 
     return (
         <Box
@@ -47,9 +49,26 @@ const Navbar = () => {
                     <IconButton sx={{ color: "black" }}>
                         <PersonOutline />
                     </IconButton>
-                    <IconButton sx={{ color: "black" }}>
-                        <ShoppingBagOutlined />
-                    </IconButton>
+                    <Badge
+                        badgeContent={cart.length}
+                        color="secondary"
+                        invisible={cart.length === 0}
+                        sx={{
+                            "& .MuiBadge-badge":{
+                                right: 5,
+                                top: 5,
+                                padding: "0 4px",
+                                height: "14px",
+                                minWidth: "13px",
+                            }
+                        }}
+                    >
+                        <IconButton
+                            onClick={() => dispatch(setIsCartOpen({}))}
+                            sx={{ color: "black" }}>
+                            <ShoppingBagOutlined />
+                        </IconButton>
+                    </Badge>
                     <IconButton sx={{ color: "black" }}>
                         <MenuOutlined />
                     </IconButton>
