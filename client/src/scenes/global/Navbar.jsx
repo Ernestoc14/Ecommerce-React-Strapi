3a5d8e90bbfad5dfb35c97e6c77f769d2e7e4439
Ernestoc14 <descreed34@hotmail.com>
2023-08-05 00:26:47 -0500

    Feat Modeling the Navbar

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -5,6 +5,57 @@ import { useNavigate } from "react-router-dom";
 import { shades } from "../../theme"
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            width="100%"
+            height="60px"
+            backgroundColor="rgba(255, 255, 255, 0.95)"
+            color="black"
+            position="fixed"
+            top="0"
+            left="0"
+            zIndex="1"
+        >
+            <Box
+                width="80%"
+                margin="auto"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Box
+                    onClick={() => navigate("/")}
+                    sx={{ '&:hover': { cursor: "pointer" } }}
+                    color={shades.secondary[500]}
+                >
+                    ECOMMER
+                </Box>
+                <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    columnGap="20px"
+                    zIndex="2"
+                >
+                    <IconButton sx={{ color: "black" }}>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton sx={{ color: "black" }}>
+                        <PersonOutline />
+                    </IconButton>
+                    <IconButton sx={{ color: "black" }}>
+                        <ShoppingBagOutlined />
+                    </IconButton>
+                    <IconButton sx={{ color: "black" }}>
+                        <MenuOutlined />
+                    </IconButton>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
 export default Navbar;
