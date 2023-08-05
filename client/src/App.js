ceb3ca90b4a5e605fcbc3acd0ac6828ba486368f
Ernestoc14 <descreed34@hotmail.com>
2023-08-05 00:12:26 -0500

    Feat Adding Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import Home from "./scenes/home/Home"
-
+import ItemDetails from "./scenes/ItemDetails/ItemDetails"
+import Checkout from "./scenes/checkout/Checkout"
+import Confirmation from "./scenes/checkout/Confirmation"
+import Navbar from "./scenes/global/Navbar"
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
@@ -12,14 +15,17 @@ const ScrollToTop = () => {
   return null
 }
 
-
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop/>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="item/:itemId" element={<ItemDetails/>} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,3 +1,9 @@
+import { useDispatch, useSelector } from "react-router-dom";
+import { Badge, Box, IconButton } from "@mui/material"
+import { PersonOutline, ShoppingBagOutlined, MenuOutlined, SearchOutlined } from "@mui/icons-material"
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme"
+
 const Navbar = () => {
     return <div>Navbar</div>;
 }
