9f4e07b416a6482cfe498b13ac4ed08960dc6895
Ernestoc14 <descreed34@hotmail.com>
2023-08-06 03:04:43 -0500

    Feat CartSideMenu Structure

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -8,8 +8,98 @@ import { shades } from "../../theme"
 import { decreaseCount, increaseCount, removeFromCart, setIsCartOpen } from "../../state"
 import { useNavigate } from "react-router-dom"
 
-const Flexbox = styled(Box)`
+const FlexBox = styled(Box)`
     display: flex;
     justify-content: space-between;
     align-items: center;
 `
+const CartMenu = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
+    const isCartOpen = useSelector((state) => state.cart.isCartOpen)
+
+    const totalPrice = cart.reduce((total, item) => {
+        return total + item.count * item.attributes.price;
+    }, 0);
+
+    return (
+        // OVERLAY
+        <Box
+            display={isCartOpen ? "block" : "none"}
+            backgroundColor="rgba(0, 0, 0, 0.4)"
+            position="fixed"
+            zIndex={10}
+            width="100%"
+            height="100%"
+            left="0"
+            top="0"
+            overflow="auto"
+        >
+            {/* MODAL */}
+            <Box
+                position="fixed"
+                right="0"
+                bottom="0"
+                width="max(400px, 30%)"
+                height=" 100%"
+                backgroundColor="white"
+            >
+                <Box
+                    padding="30px" overflow="auto" height="30%">
+                    {/* HEADER */}
+                    <FlexBox mb="15px">
+                        <Typography variant="h3">SHOPPING BAG ({cart.lenght})</Typography>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                            <CloseIcon />
+                        </IconButton>
+                    </FlexBox>\
+                    {/* CART LIST */}
+                    <Box>
+                        {cart.map((item) =>(
+                            <Box key={`${item.attributes.name}-${item.id}`}>
+                                <FlexBox p="15px 0">
+                                    <Box flex="1 1 40%">
+                                        <img
+                                            alt={item?.name}
+                                            width="123px"
+                                            height="164px"
+                                            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                                        />
+                                    </Box>
+                                    <Box flex="1 1 40%">
+                                        {/* ITEM NAME */}
+                                        <FlexBox mb="5px">
+                                            <Typography fontWeight="bold">
+                                                {item.attributes.name}
+                                            </Typography>
+                                            <IconButton onClick={() => dispatch(removeFromCart({ id: item.id}))}>
+                                                <CloseIcon/>
+                                            </IconButton>
+                                        </FlexBox>
+                                        <Typography>{item.attributes.shortDescription}</Typography>
+                                        {/* AMOUNT */}
+                                        <FlexBox m="15px 0">
+                                            <Box
+                                                display="flex"
+                                                alignItems="center"
+                                                border={`1.5px solid ${shades.neutral[500]}`}
+                                            >
+                                                <IconButton
+                                                    onClick={() => dispatch(decreaseCount({ id: item.id }))}
+                                                >
+                                                    
+                                                </IconButton>
+                                            </Box>
+                                        </FlexBox>
+                                    </Box>
+                                </FlexBox>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+
+            </Box>
+        </Box>
+    )
+}
