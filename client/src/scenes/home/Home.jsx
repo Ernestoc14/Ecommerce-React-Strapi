92ef472f96d35cc97aa28dbdf9790c2a9018a6d3
Ernestoc14 <descreed34@hotmail.com>
2023-08-08 02:07:45 -0500

    Feat Adding Responsive Carousel to Home Page

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,4 +1,14 @@
+import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
+
+// NOT RENDERING THE CAROUSEL
 const Home = () => {
-    return <div>Home</div>;
-}
+    return(
+        <div className="home">
+            <MainCarousel />
+            <ShoppingList />
+        </div>    
+    );
+};
+
 export default Home;
