5394c77847723a2dd107c02dd5c770196c0aaf97
Ernestoc14 <descreed34@hotmail.com>
2023-08-09 00:48:19 -0500

    Fix Bugs in Item Component

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -7,6 +7,30 @@ import { setItems } from "../../state"
 const ShoppingList = () => {
     const dispatch = useDispatch()
     const [value, setValue] = useState("all")
+    const items = useSelector((state) => state.cart.items)
+    const isNonMobile = useMediaQuery("(min-width:600px)")
+    console.log("items", items)
+
+    const handleChange = ( event, newValue ) => {
+        setValue(newValue)
+    }
+
+    async function getItems() {
+        const items = await fetch(
+            "http://localhost:1337/api/items?populate=image",
+            { method: "GET" }
+        )
+        const itemsJson = await items.json()
+        dispatch(setItems(itemsJson.data))
+    }
+
+    useEffect(() => {
+        getItems();
+    }, [])
+
+    return (
+        <div> Shopping List </div>
+    )
 }
 
 export default ShoppingList
