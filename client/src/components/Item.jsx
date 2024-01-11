579088eb83504ec11610c43042b905b2d5d83fe7
Ernestoc14 <descreed34@hotmail.com>
2024-01-10 23:23:16 -0500

    Fix: Display property in Item component to render "Add to Cart" button correctly

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -43,7 +43,7 @@ const Item = ({ item, width }) => {
                     style={{ cursor: 'pointer' }}
                 />
                 <Box
-                    display={isHovered ? "blocked" : "none"}
+                    display={isHovered ? "block" : "none"}
                     position="absolute"
                     bottom="10%"
                     left="0"
