4978fefa9a6e1e8b9f123852daf657a5edc09150
Ernestoc14 <descreed34@hotmail.com>
2024-01-11 01:22:53 -0500

    Feat: Add Email Feature in HomePage

--- client/src/scenes/home/Subscribe.jsx
+++ client/src/scenes/home/Subscribe.jsx
@@ -12,6 +12,25 @@ const Subscribe = () => {
       </IconButton>
       <Typography variant="h3">Subscribe To Our Newsletter</Typography>
       <Typography variant="subtitle1">Get the latest news on our products.</Typography>
+      <Box 
+        p="2px 4px"
+        m="15px auto"
+        display="flex"
+        alignItems="center"
+        width="75%"
+        backgroundColor="#F2F2F2"
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder='Enter your email address'
+          onChange={(e) => (setEmail(e.target.value))}
+          value={email}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <Typography sx={{ p: "10px", ":hover": {cursor: "pointer"} }}>Subscribe</Typography>
+      </Box>
     </Box>
   )
 }
+
+export default Subscribe;
