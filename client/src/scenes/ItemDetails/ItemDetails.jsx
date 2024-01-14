517cefe5b5f60c2dfd2a64e0d4ddb4d718bb0f5d
Ernestoc14 <descreed34@hotmail.com>
2024-01-13 23:59:05 -0500

    Feat: Starting the UI in ItemDetails Page

--- client/src/scenes/ItemDetails/ItemDetails.jsx
+++ client/src/scenes/ItemDetails/ItemDetails.jsx
@@ -45,6 +45,33 @@ const ItemDetails = () => {
       getItems();
     }, [itemId]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <div>ItemDetails</div>;
+  return <Box
+    width="80%" m="80px auto">
+    <Box display="flex" flexWrap="wrap" columnGap="40px">
+        {/* IMAGES*/}
+        <Box flex="1 1 40%" mb="40px">
+            <img
+                alt={item?.name}
+                width="100%"
+                height="100%"
+                src={`http://localhost:3001${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                style={{ objectFit: "contain" }}
+            />
+        </Box>
+        {/* ACTIONS */}
+        <Box flex="1 1 50%" mb="40px">
+            <Box>Home / Item</Box>
+            <Box>Prev Next</Box>
+        </Box>
+
+        <Box m="65px 0 25px 0">
+            <Typography variant="h3" >{items?.attributes?.names}</Typography>
+            <Typography>{items?.attributes?.price}</Typography>
+            <Typography sx={{ mt: '20px'}}>
+                {items?.attributes?.longDescription}
+            </Typography>
+        </Box>
+    </Box>
+  </Box>
 };
 export default ItemDetails;
