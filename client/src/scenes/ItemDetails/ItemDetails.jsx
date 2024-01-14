bd1563aa7a1a4a5cfb8a7e567f45fcd4cf127920
Ernestoc14 <descreed34@hotmail.com>
2024-01-13 23:46:16 -0500

    Feat: Adding Functionality Features to ItemDetails Page

--- client/src/scenes/ItemDetails/ItemDetails.jsx
+++ client/src/scenes/ItemDetails/ItemDetails.jsx
@@ -1,4 +1,50 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { IconButton, Box, Typography, Button, Tabs, Tab } from "@mui/material";
+import { FavoriteBorderOutlined } from "@mui/icons-material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../../theme";
+import { addToCart } from "../../state";
+import Item from "../../components/Item";
+
 const ItemDetails = () => {
-    return<div>Item Details</div>
-}
+  const dispatch = useDispatch();
+  const { itemId } = useParams();
+  const [value, setValue] = useState("description");
+  const [count, setCount] = useState(1);
+  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItem() {
+    const item = await fetch(
+      `http://localhost:3001/api/items/${itemId}?populate=image`,
+      { method: "GET" }
+    );
+    const itemJson = await item.json();
+    setItem(itemJson.data);
+  }
+
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:3001/api/items?populate=image",
+      { method: "GET" }
+    );
+    const itemsJson = await items.json();
+    setItems(itemsJson.data);
+  }
+
+
+    useEffect(() => {
+      getItem();
+      getItems();
+    }, [itemId]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div>ItemDetails</div>;
+};
 export default ItemDetails;
