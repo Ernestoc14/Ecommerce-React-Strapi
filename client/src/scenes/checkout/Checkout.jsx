3602cd33e855fa9f4c8840ab38a26c3a676338b9
Ernestoc14 <descreed34@hotmail.com>
2024-01-17 23:51:46 -0500

    Feat: Add multi-step form functionality to Checkout component (Not Finished Yet)

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,4 +1,38 @@
+import { useSelector } from 'react-redux';
+import { Box, Button, Stepper, Step, StepLabel } from "@mui/material"
+import { Formik } from 'formik';
+import { useState } from 'react';
+import * as yup from "yup"
+import { shades } from '../../theme';
+
+
 const Checkout = () => {
-    return<div>Checkout</div>
+    const [activeStep, setActiveStep] = useState(0)
+    const cart = useSelector((state) => state.cart.cart)
+    const isFirstStep = activeStep === 0
+    const isSecondStep = activeStep === 1
+
+    const handleFormSubmit = async(value, actions) => {
+        setActiveStep(activeStep + 1)
+    }
+
+    async function makePayment(values) {
+
+    }
+    return <Box>
+        <Stepper activeStep={activeStep} sx={{ m: "20px 0"}}>
+            <Step>
+                <StepLabel>Billing</StepLabel>
+            </Step>
+            <Step>
+                <StepLabel>Payment</StepLabel>
+            </Step>
+        </Stepper>
+        <Box>
+            
+        </Box>
+    </Box>
 }
 export default Checkout;
+
+// 2 hr 53 min
