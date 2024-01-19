555056b289fdf024fb99d4449da8d9966e548ffd
Ernestoc14 <descreed34@hotmail.com>
2024-01-18 23:42:16 -0500

    Add form validation with Yup and initial values to Checkout component

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -5,34 +5,77 @@ import { useState } from 'react';
 import * as yup from "yup"
 import { shades } from '../../theme';
 
+const initialValues = {
+  billingAddress: {
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  shippingAddress: {
+    isSameAddress: true,
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  email: "",
+  phoneNumber: ""
+}
+
+const checkoutSchema = [
+  yup.object().shape({
+    billingAddress: yup.object().shape({
+      firstName: yup.string().required("required"),
+      lastName: yup.string().required("required"),
+      country: yup.string().required("required"),
+      street1: yup.string().required("required"),
+      street2: yup.string(),
+      city: yup.string().required("required"),
+      state: yup.string().required("required"),
+      zipCode: yup.string().required("required"),
+    }),
+  }),
+  // 3 HR
+]
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(0)
-    const cart = useSelector((state) => state.cart.cart)
-    const isFirstStep = activeStep === 0
-    const isSecondStep = activeStep === 1
+  const [activeStep, setActiveStep] = useState(0)
+  const cart = useSelector((state) => state.cart.cart)
+  const isFirstStep = activeStep === 0
+  const isSecondStep = activeStep === 1
 
-    const handleFormSubmit = async(value, actions) => {
-        setActiveStep(activeStep + 1)
-    }
+  const handleFormSubmit = async (value, actions) => {
+    setActiveStep(activeStep + 1)
+  }
 
-    async function makePayment(values) {
+  async function makePayment(values) {
 
-    }
-    return <Box>
-        <Stepper activeStep={activeStep} sx={{ m: "20px 0"}}>
-            <Step>
-                <StepLabel>Billing</StepLabel>
-            </Step>
-            <Step>
-                <StepLabel>Payment</StepLabel>
-            </Step>
-        </Stepper>
-        <Box>
-            
-        </Box>
+  }
+  return <Box>
+    <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
+      <Step>
+        <StepLabel>Billing</StepLabel>
+      </Step>
+      <Step>
+        <StepLabel>Payment</StepLabel>
+      </Step>
+    </Stepper>
+    <Box>
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      ></Formik>
     </Box>
+  </Box>
 }
 export default Checkout;
-
-// 2 hr 53 min
