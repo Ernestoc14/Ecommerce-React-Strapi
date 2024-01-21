c2d18dd608f67d54c6fcfbb65158a26522673823
Ernestoc14 <descreed34@hotmail.com>
2024-01-20 21:04:32 -0500

    Feat: Refactor Checkout component and form validation using Yup

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,9 +1,9 @@
-import { useSelector } from 'react-redux';
-import { Box, Button, Stepper, Step, StepLabel } from "@mui/material"
+// import { useSelector } from 'react-redux';
+import { Box, Stepper, Step, StepLabel } from "@mui/material"
 import { Formik } from 'formik';
 import { useState } from 'react';
 import * as yup from "yup"
-import { shades } from '../../theme';
+// import { shades } from '../../theme';
 
 const initialValues = {
   billingAddress: {
@@ -49,31 +49,53 @@ const checkoutSchema = [
         is: false, 
         then: yup.string().required("required"),
       }),
-      lastName: yup.string().required("required"),
-      country: yup.string().required("required"),
-      street1: yup.string().required("required"),
+      lastName: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
+      country: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
+      street1: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
       street2: yup.string(),
-      city: yup.string().required("required"),
-      state: yup.string().required("required"),
-      zipCode: yup.string().required("required"),
+      city: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
+      state: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
+      zipCode: yup.string().when("isSameAddress", {
+        is: false, 
+        then: yup.string().required("required"),
+      }),
     }),
   }),
+  yup.object().shape({
+    email: yup.string().required("required"),
+    phoneNumber: yup.string().required("required")
+  })
   // 3 HR
 ]
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0)
-  const cart = useSelector((state) => state.cart.cart)
-  const isFirstStep = activeStep === 0
-  const isSecondStep = activeStep === 1
+  // const cart = useSelector((state) => state.cart.cart)
+  // const isFirstStep = activeStep === 0
+  // const isSecondStep = activeStep === 1
 
   const handleFormSubmit = async (value, actions) => {
     setActiveStep(activeStep + 1)
   }
 
-  async function makePayment(values) {
+  // async function makePayment(values) {
 
-  }
+  // }
   return <Box>
     <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
       <Step>
@@ -87,7 +109,7 @@ const Checkout = () => {
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
-        validationSchema={validationSchema}
+        validationSchema={checkoutSchema[activeStep]}
       ></Formik>
     </Box>
   </Box>
