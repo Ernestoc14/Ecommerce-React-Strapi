4b5fcc64c45c88a9144d2b32713cd90ba9200838
Ernestoc14 <descreed34@hotmail.com>
2024-01-21 23:41:42 -0500

    Feat: Refactor Shipping component to accept additional props

--- client/src/scenes/checkout/Shipping.jsx
+++ client/src/scenes/checkout/Shipping.jsx
@@ -1,6 +1,12 @@
 import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material"
 import AddressForm from "./AddressForm"
 
-const Shipping = () => {
+const Shipping = (values,
+  errors,
+  touched,
+  handleBlur,
+  handleChange,
+  setFieldValue,
+) => {
 
 }
