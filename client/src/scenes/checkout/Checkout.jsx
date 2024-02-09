e95ce6d646019515a1670a5d52931875fcc07ed6
Ernestoc14 <descreed34@hotmail.com>
2024-02-08 23:36:05 -0500

    Feat: Add back and next button on Checkout component

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -145,6 +145,37 @@ const Checkout = () => {
                 setFieldValue={setFieldValue}
               />
             )}
+            <Box display="flex" justifyContent="space-between" gap="50px">
+              {isSecondStep && (
+                <Button
+                  fullWidth
+                  color='primary'
+                  variant='contained'
+                  sx={{
+                    backgroundColor: shades.primary[200],
+                    boxShadow: "none",
+                    color: "white",
+                    borderRadius: 0,
+                    padding: "15px 40px"
+                  }}
+                  onClick={() => setActiveStep(activeStep - 1)}
+                >Back</Button>
+              )}
+              <Button
+                fullWidth
+                type='submit'
+                color="primary"
+                variant="contained"
+                sx={{
+                  backgroundColor: shades.primary[400],
+                  boxShadow: "none",
+                  color: "white",
+                  borderRadius: 0,
+                  padding: "15px 40px"
+                }}
+                onClick={() => setActiveStep(activeStep + 1)}
+              >{isFirstStep ? "Next" : "Place Order"}</Button>
+            </Box>
           </form>
         )}
       </Formik>
