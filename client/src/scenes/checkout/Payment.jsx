def371e03c109cbcb26cf9f0af39bda18ab88cad
Ernestoc14 <descreed34@hotmail.com>
2024-02-17 18:48:40 -0500

    Feat: Wrapping TextFields and Typography onto a Box in Payment Component

--- client/src/scenes/checkout/Payment.jsx
+++ client/src/scenes/checkout/Payment.jsx
@@ -4,33 +4,35 @@ const Payment = ({ values, touched, errors, handleBlur, handleChange }) => {
   return (
     <Box m="30px 0">
       {/* CONTACT INFO */}
-      <Typography sx={{ mb: "15px" }} fontSize="18px">
-        Contact Information
-      </Typography>
-      <TextField
-        fullWidth
-        label="Email"
-        type="text"
-        value={values.email}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        name='email'
-        error={!!touched.email && !!errors.email}
-        helperText={touched.email && errors.email}
-        sx={{ gridColumn: "span 4", marginBottom: "15px" }}
-      />
-      <TextField
-        fullWidth
-        label="Phone Number"
-        type="text"
-        value={values.phoneNumber}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        name='phoneNumber'
-        error={!!touched.phoneNumber && !!errors.phoneNumber}
-        helperText={touched.phoneNumber && errors.phoneNumber}
-        sx={{ gridColumn: "span 4" }}
-      />
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Contact Information
+        </Typography>
+        <TextField
+          fullWidth
+          label="Email"
+          type="text"
+          value={values.email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          name='email'
+          error={!!touched.email && !!errors.email}
+          helperText={touched.email && errors.email}
+          sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+        />
+        <TextField
+          fullWidth
+          label="Phone Number"
+          type="text"
+          value={values.phoneNumber}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          name='phoneNumber'
+          error={!!touched.phoneNumber && !!errors.phoneNumber}
+          helperText={touched.phoneNumber && errors.phoneNumber}
+          sx={{ gridColumn: "span 4" }}
+        />
+      </Box>
     </Box >
   )
 }
