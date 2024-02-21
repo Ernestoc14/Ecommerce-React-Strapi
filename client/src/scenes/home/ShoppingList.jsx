08453b3e72cfa8186f6d878150d4cc04d6ca4ba7
Ernestoc14 <descreed34@hotmail.com>
2024-02-20 22:03:58 -0500

    Feat: Remove console.log in ShoppingList Component

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -9,7 +9,6 @@ const ShoppingList = () => {
     const [value, setValue] = useState("all")
     const items = useSelector((state) => state.cart.items)
     const isNonMobile = useMediaQuery("(min-width:600px)")
-    console.log("items", items)
 
     const handleChange = (event, newValue) => {
         setValue(newValue)
