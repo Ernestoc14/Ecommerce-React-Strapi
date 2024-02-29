e315b0cc28f2a67b22d91e702a57d8bed55723a8
Ernestoc14 <descreed34@hotmail.com>
2024-02-28 20:58:39 -0500

    Reformatting Files

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -8,91 +8,91 @@ import { shades } from "../theme"
 import { addToCart } from "../state"
 
 const Item = ({ item, width }) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [count, setCount] = useState(1);
-    const [isHovered, setIsHovered] = useState(false);
-    const {
-        palette: { neutral },
-    } = useTheme();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+  const {
+    palette: { neutral },
+  } = useTheme();
 
-    const { category, price, name, image } = item.attributes;
-    const {
-        data: {
-            attributes: {
-                formats: {
-                    medium: { url },
-                }
-            }
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formats: {
+          medium: { url },
         }
-    } = image;
+      }
+    }
+  } = image;
 
-    return (
-        <Box width={width}>
+  return (
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+      >
+        <img
+          alt={item.name}
+          width="300px"
+          height="400px"
+          src={`http://localhost:1337${url}`}
+          onClick={() => navigate(`/item/${item.id}`)}
+          style={{ cursor: 'pointer' }}
+        />
+        <Box
+          display={isHovered ? "block" : "none"}
+          position="absolute"
+          bottom="10%"
+          left="0"
+          width="100%"
+          padding="0 5%"
+        >
+          <Box display="flex" justifyContent="space-between">
+            {/* AMOUNT */}
             <Box
-                position="relative"
-                onMouseOver={() => setIsHovered(true)}
-                onMouseOut={() => setIsHovered(false)}
+              display="flex"
+              alignItems="center"
+              backgroundColor={shades.neutral[100]}
+              borderRadius="3px"
             >
-                <img
-                    alt={item.name}
-                    width="300px"
-                    height="400px"
-                    src={`http://localhost:1337${url}`}
-                    onClick={() => navigate(`/item/${item.id}`)}
-                    style={{ cursor: 'pointer' }}
-                />
-                <Box
-                    display={isHovered ? "block" : "none"}
-                    position="absolute"
-                    bottom="10%"
-                    left="0"
-                    width="100%"
-                    padding="0 5%"
-                >
-                    <Box display="flex" justifyContent="space-between">
-                        {/* AMOUNT */}
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            backgroundColor={shades.neutral[100]}
-                            borderRadius="3px"
-                        >
-                            <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
-                                <RemoveIcon />
-                            </IconButton>
-                            <Typography color={shades.primary[300]}>{count}</Typography>
-                            <IconButton onClick={() => setCount(count + 1)}>
-                                <AddIcon />
-                            </IconButton>
-                        </Box>
-                        {/* BUTTON */}
-                        <Button
-                            onClick={() => {
-                                dispatch(addToCart({ item: { ...item, count } }))
-                            }}
-                            sx={{
-                                backgroundColor: shades.primary[300], color: "white",
-                                "&:hover": {
-                                    backgroundColor: shades.neutral[300],
-                                    color: "black"
-                                }
-                            }}
-                        >
-                            Add to Cart
-                        </Button>
-                    </Box>
-                </Box>
+              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography color={shades.primary[300]}>{count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
             </Box>
-            <Box mt="3px">
-                <Typography variant="subtitle2" color={neutral.dark}>
-                    {category.replace(/([A-Z])/g, "$1").replace(/^./, (str) => str.toUpperCase())}
-                </Typography>
-                <Typography>{name}</Typography>
-                <Typography fontWeight="bold">${price}</Typography>
-            </Box>
+            {/* BUTTON */}
+            <Button
+              onClick={() => {
+                dispatch(addToCart({ item: { ...item, count } }))
+              }}
+              sx={{
+                backgroundColor: shades.primary[300], color: "white",
+                "&:hover": {
+                  backgroundColor: shades.neutral[300],
+                  color: "black"
+                }
+              }}
+            >
+              Add to Cart
+            </Button>
+          </Box>
         </Box>
-    )
+      </Box>
+      <Box mt="3px">
+        <Typography variant="subtitle2" color={neutral.dark}>
+          {category.replace(/([A-Z])/g, "$1").replace(/^./, (str) => str.toUpperCase())}
+        </Typography>
+        <Typography>{name}</Typography>
+        <Typography fontWeight="bold">${price}</Typography>
+      </Box>
+    </Box>
+  )
 }
 
 export default Item
--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -5,81 +5,81 @@ import Item from "../../components/Item"
 import { setItems } from "../../state"
 
 const ShoppingList = () => {
-    const dispatch = useDispatch()
-    const [value, setValue] = useState("all")
-    const items = useSelector((state) => state.cart.items)
-    const isNonMobile = useMediaQuery("(min-width:600px)")
+  const dispatch = useDispatch()
+  const [value, setValue] = useState("all")
+  const items = useSelector((state) => state.cart.items)
+  const isNonMobile = useMediaQuery("(min-width:600px)")
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue)
-    }
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
 
-    async function getItems() {
-        const items = await fetch(
-            "http://localhost:1337/api/items?populate=image",
-            { method: "GET" }
-        )
-        const itemsJson = await items.json()
-        dispatch(setItems(itemsJson.data))
-    }
-
-    useEffect(() => {
-        getItems();
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-    const topRatedItems = items.filter((item) => item.attributes.category === "topRated")
-
-    const newArrivalsItems = items.filter((item) => item.attributes.category === "newArrivals")
-
-    const bestSellersItems = items.filter((item) => item.attributes.category === "bestSellers")
-
-    return (
-        <Box width="80%" margin="80px auto">
-            <Typography variant="h3" textAlign="center">
-                Our Featured <b>Products</b>
-            </Typography>
-            <Tabs
-                textColor='primary'
-                indicatorColor='primary'
-                value={value}
-                onChange={handleChange}
-                centered
-                TabIndicatorProps={{ sx: { display: isNonMobile ? "block" : "none" } }}
-                sx={{
-                    m:"25px",
-                    "& .MuiTabs-flexContainer": {
-                        flexWrap: "wrap",
-                    }
-                }}
-            >
-                <Tab label="All" value="all" />
-                <Tab label="Top Rated" value="topRated" />
-                <Tab label="New Arrivals" value="newArrivals" />
-                <Tab label="Best Sellers" value="bestSellers" />
-            </Tabs>
-            <Box 
-                margin="0 auto"
-                display="grid"
-                gridTemplateColumns="repeat(auto-fit, 250px)"
-                justifyContent="space-around"
-                rowGap="20px"
-                columnGap="1.33%"
-            >
-                {value === "all" && items.map((item) => (
-                    <Item item={item} key={`${item.name}-${item.id}`} />
-                ))}
-                {value === "newArrivals" && newArrivalsItems.map((item) => (
-                    <Item item={item} key={`${item.name}-${item.id}`} />
-                ))}
-                {value === "bestSellers" && bestSellersItems.map((item) => (
-                    <Item item={item} key={`${item.name}-${item.id}`} />
-                ))}
-                {value === "topRated" && topRatedItems.map((item) => (
-                    <Item item={item} key={`${item.name}-${item.id}`} />
-                ))}
-            </Box>
-        </Box>
+  async function getItems() {
+    const items = await fetch(
+      "http://localhost:1337/api/items?populate=image",
+      { method: "GET" }
     )
+    const itemsJson = await items.json()
+    dispatch(setItems(itemsJson.data))
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const topRatedItems = items.filter((item) => item.attributes.category === "topRated")
+
+  const newArrivalsItems = items.filter((item) => item.attributes.category === "newArrivals")
+
+  const bestSellersItems = items.filter((item) => item.attributes.category === "bestSellers")
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Our Featured <b>Products</b>
+      </Typography>
+      <Tabs
+        textColor='primary'
+        indicatorColor='primary'
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{ sx: { display: isNonMobile ? "block" : "none" } }}
+        sx={{
+          m: "25px",
+          "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+          }
+        }}
+      >
+        <Tab label="All" value="all" />
+        <Tab label="Top Rated" value="topRated" />
+        <Tab label="New Arrivals" value="newArrivals" />
+        <Tab label="Best Sellers" value="bestSellers" />
+      </Tabs>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fit, 250px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {value === "all" && items.map((item) => (
+          <Item item={item} key={`${item.name}-${item.id}`} />
+        ))}
+        {value === "newArrivals" && newArrivalsItems.map((item) => (
+          <Item item={item} key={`${item.name}-${item.id}`} />
+        ))}
+        {value === "bestSellers" && bestSellersItems.map((item) => (
+          <Item item={item} key={`${item.name}-${item.id}`} />
+        ))}
+        {value === "topRated" && topRatedItems.map((item) => (
+          <Item item={item} key={`${item.name}-${item.id}`} />
+        ))}
+      </Box>
+    </Box>
+  )
 }
 
 export default ShoppingList
