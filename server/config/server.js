ca9e7309aac19bd13a791b90eef6796594b0d9b3
Ernestoc14 <descreed34@hotmail.com>
2024-02-28 21:43:35 -0500

    Feat: Adding CORS to Server Side

--- server/config/server.js
+++ server/config/server.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+  origin: ["https:ecommerce-react-strapi.vercel.app", "http://localhost:3000"],
+}))
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
